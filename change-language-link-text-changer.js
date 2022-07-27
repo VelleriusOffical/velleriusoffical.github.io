6e905c73542c29e494194a0e3dcf96198bfacea2
VelleriusOffical <109928141+VelleriusOffical@users.noreply.github.com>
2022-07-27 09:17:04 -0700

    Delete change-language-link-text-changer.js

--- change-language-link-text-changer.js
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript">  
-    var change_language_link = "https://www.velleriushosting.me",
-    change_language_text = "Change Language";
-</script>  
-<script src="https://velleriusoffical.github.io/change-language-link-text-changer.js" type="text/javascript"></script>  
