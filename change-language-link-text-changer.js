7cbeddfea21340e00d0095fe0f6fc80e1fd9639d
VelleriusOffical <109928141+VelleriusOffical@users.noreply.github.com>
2022-07-27 09:14:48 -0700

    Create change-language-link-text-changer.js

---
+++ change-language-link-text-changer.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">  
+    var change_language_link = "https://client.velleriushosting.me",
+    change_language_text = "Change Language";
+</script>  
+<script src="https://velleriusoffical.github.io/change-language-link-text-changer.js" type="text/javascript"></script>  
