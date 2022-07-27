4a9c8f73b7a7a43965f25330b5129e2dfacedbfa
VelleriusOffical <109928141+VelleriusOffical@users.noreply.github.com>
2022-07-27 09:15:48 -0700

    Create change-password-link-text-changer.js

---
+++ change-password-link-text-changer.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">  
+    var change_password_link = "https://www.velleriushosting.me",
+    change_password_text = "your_text";
+</script>  
+<script src="https://velleriusoffical.github.io/change-password-link-text-changer.js" type="text/javascript"></script>  
